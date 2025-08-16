8684d6b31d19cda1fd9762b3b6459b0e8d2640ee
alfathroziqq <alfathroziq94@gmail.com>
2025-08-16 14:21:38 +0700

    fix-bg-welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -113,7 +113,7 @@
     <!-- Hero -->
     <section class="relative min-h-screen w-full -mt-20 flex items-center justify-center lg:justify-start">
         <div class="absolute inset-0 bg-cover bg-[center_top_30%]"
-            style="background-image: url('{{ asset('/public/images/timpik_sawah2.jpg') }}');">
+            style="background-image: url('{{ asset('images/timpik_sawah2.jpg') }}');">
         </div>
 
         <div class="absolute inset-0 bg-gradient-to-t from-[#0C3B2E]/60 via-transparent to-transparent"></div>
